b7abe8cf4b689ea5fbd7fb4c79275c840a3d2c57
filippob <filippo.biscarini@gmail.com>
2022-07-11 16:20:35 +0200

    first scripts for qiime 1.9

---
+++ qiime1.9/1.extract_barcode.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+## script that removes barcodes from already demultiplexed data
+## input data format is fastq: R1 and R2 files (forward and reverse)
+## by default the first 8 bases are trimmed: otherwise, pass a manifest/mapping file with the barcode sequences
+
+## setting up the environment
+currpath=$(pwd)
+project_home="$HOME/useful_microbiome_predipping"
+#data_folder="data/220225_M04028_0144_000000000-K6CMG"
+output_dir="Analysis/qiime1.9/1.extract_barcode"
+#sing_container="${project_home}/Qiime1.9.sif"
+sing_container="$HOME/software/qiime_docker:fischuu-qiime-1.9.1.sif"
+temp_folder="$HOME/temp"
+
+cd $project_home
+echo "project folder is $project_home"
+
+## make output folder if it does not exist
+if [ ! -d "${output_dir}" ]; then
+	mkdir -p ${output_dir}
+	chmod g+rxw ${output_dir}
+fi
+
+## using the Singularity container
+echo " - calling the singularity container"
+singularity run ${sing_container} multiple_extract_barcodes.py --input_dir=${temp_folder} --output_dir=${output_dir} --read1_indicator _R1 --read2_indicator _R2
+
+echo " - removing barcodes files"
+cd $output_dir
+find . -name \*barcodes.fastq -type f -delete
+cd $currpath
+
+echo " - removing temprorary files"
+rm ${temp_folder}/*
+
+echo "DONE!"
+
---
+++ qiime1.9/2.join_paired_ends.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+## script that joins paired end reads after the barcode has been removed
+## input data format is fastq: R1 and R2 files (forward and reverse) from 1.extract_barcode.sh
+## 
+
+## setting up the environment
+currpath=$(pwd)
+project_home="$HOME/useful_microbiome_predipping"
+input_folder="Analysis/qiime1.9/1.extract_barcode"
+output_dir="Analysis/qiime1.9/2.join_paired_ends"
+#sing_container="${project_home}/Qiime1.9.sif"
+sing_container="$HOME/software/qiime_docker:fischuu-qiime-1.9.1.sif"
+paramfile="Config/join.parameters"
+
+cd $currpath
+echo "project folder is $project_home"
+
+## make folder if it does not exist
+if [ ! -d "${output_dir}" ]; then
+	mkdir -p ${output_dir}
+	chmod g+rxw ${output_dir}
+fi
+
+## generating qiime_parameters file
+#echo "join_paired_ends:pe_join_method SeqPrep" >| qiime_parameters_joining 
+
+## using the Singularity container
+echo " - calling the singularity container for read joining"
+singularity run ${sing_container} multiple_join_paired_ends.py --input_dir=${input_folder} --output_dir=${output_dir} --include_input_dir_path --parameter_fp=$paramfile --read1_indicator=_R1 --read2_indicator=_R2
+chmod g+rxw -R ${output_dir}
+
+echo " - removing unassembled reads"
+cd $output_dir
+find . -name \*unassembled*.fastq.gz -type f -delete
+cd $currpath
+
+## clean output folder from potential barcode subfolders
+echo " - cleaning the output folder (barcodes subfolders)"
+rm -r ${output_dir}/*barcodes
+
+echo "DONE!"
+
