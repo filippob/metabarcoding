0e44b874ab7cc5b91318829b8b1a56a472cb1d24
filippob <filippo.biscarini@gmail.com>
2022-10-27 11:01:28 +0200

    adding controls to use singularity containers if needed (e.g. minicluster/slurm)

--- 0.fastqc.sh
+++ 0.fastqc.sh
@@ -8,16 +8,21 @@
 
 ## folder paths
 currpath=$(pwd)
-datapath="/home/ngs/Macrogen/1.Rawdata/"
-outdir="useful_microbiome_chlorine/Analysis/0.fastqc"
-temp_folder="$HOME/temp"
+datapath="/home/ngs/220713_M04028_0149_000000000-KFG5W/"
+outdir="$HOME/bontempo_pigs_rectum/Analysis/0.fastqc"
+temp_folder="$HOME/temp_microbiome"
 
 ## samples to select
-sample_start= #first sample to use (in the sequence)
-sample_end= #last sample to use (in the sequence)
-prefix="RAL"
+sample_start=15 #first sample to use (in the sequence)
+sample_end=104 #last sample to use (in the sequence)
+prefix=""
+
+## software
+fastqcsing="/home/core/nxf_singularity_cache/depot.galaxyproject.org-singularity-fastqc-0.11.9--0.img"
+multiqcsing="/home/core/nxf_singularity_cache/quay.io-biocontainers-multiqc-1.9--py_1.img"
 
 ## computing parameters
+singularity=1 ## 1 to use the singularity containers (slurm on minicluster); 0 to use local software
 core=8
 condapath="/usr/local/Miniconda3-py38_4.8.3-Linux-x86_64/etc/profile.d/conda.sh" ## to export conda functions (e.g. activate/deactivate)
 condaenv="mycobiota"
@@ -48,7 +53,13 @@ fi
 
 ## FastQC
 echo " - running FastQC"
-$HOME/software/FastQC/fastqc ${temp_folder}/*.fastq.gz -o ${temp_folder} -t 8
+if [ $singularity == 1 ]; then
+	echo "running FASTQC through singularity"
+	$fastqcsing fastqc ${temp_folder}/*.fastq.gz -o ${temp_folder} -t 8
+else
+	echo "running FASTQC from locally installed software"
+	$HOME/software/FastQC/fastqc ${temp_folder}/*.fastq.gz -o ${temp_folder} -t 8
+fi
 
 ## MULTI-QC
 cd $currpath
@@ -56,11 +67,19 @@ if [ ! -d $outdir ]; then
 	mkdir -p $outdir
 fi
 
-echo " - activate the conda env and run multiqc"
-source $condapath
-conda activate $condaenv
 cd ${temp_folder}
-multiqc .
+
+if [ $singularity == 1 ]; then
+	echo "running MULTIQC through singularity"
+	$multiqcsing multiqc .
+else
+	echo " - activate the conda env and run multiqc"
+	source $condapath
+	conda activate $condaenv
+
+	echo "running MULTIQC through locally installed software"
+	multiqc .
+fi
 
 cp multiqc_report.html ../$outdir/
 cp -r multiqc_data ../$outdir/
