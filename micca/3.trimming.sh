af0dd132b8839170ad31045ba050925849e1aca1
filippob <filippo.biscarini@gmail.com>
2022-10-28 09:32:31 +0200

    specifying read indicators as variables

--- micca/3.trimming.sh
+++ micca/3.trimming.sh
@@ -2,10 +2,13 @@
 
 ## setting the enviornmnent
 currpath=$(pwd)
-project_home="$HOME/bontempo_pigs_rectum"
+project_home="$HOME/bontempo_pigs_caecum_gastroherb"
 outdir="${project_home}/Analysis/micca"
 core=8
 sickle_exe="$HOME/software/sickle/sickle"
+
+r1="R1" ## delimiter R1
+r2="R2" ## delimiter R2
 
 if [ ! -d "${outdir}/trimmed" ]; then
 	mkdir -p ${outdir}/trimmed
@@ -22,7 +25,7 @@ do
 	echo "Running sickle on file "${file}""
 	echo "Running sickle on file "${file}"" >> ${outdir}/quality_control/stats_trim.txt
 	## !!! watch out with the read indicator (e.g. R1/R2 or 1/2) !!!
-	$sickle_exe pe -f "${file}_R1_cutadapt.fastq.gz" -r "${file}_R2_cutadapt.fastq.gz" -o ${outdir}/trimmed/"${file}_trimmed_R1.fastq.gz" -p ${outdir}/trimmed/"${file}_trimmed_R2.fastq.gz" -s ${outdir}/trimmed/"${file}_singles.gz" -t sanger -q 20 -g 1>> ${outdir}/quality_control/stats_trim.txt
+	$sickle_exe pe -f "${file}_${r1}_cutadapt.fastq.gz" -r "${file}_${r2}_cutadapt.fastq.gz" -o ${outdir}/trimmed/"${file}_trimmed_${r1}.fastq.gz" -p ${outdir}/trimmed/"${file}_trimmed_${r2}.fastq.gz" -s ${outdir}/trimmed/"${file}_singles.gz" -t sanger -q 20 -g 1>> ${outdir}/quality_control/stats_trim.txt
 done < names_single.txt
 
 echo "DONE!!"
