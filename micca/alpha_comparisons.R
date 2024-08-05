560743381f58e2ee97331c9eacfec1d91bb91d0f
filippob <filippo.biscarini@gmail.com>
2024-08-05 19:52:03 +0200

    added: check on the existance of the timepoint effect

--- micca/alpha_comparisons.R
+++ micca/alpha_comparisons.R
@@ -32,17 +32,17 @@ if (length(args) >= 1) {
     #base_folder = '~/Documents/SMARTER/Analysis/hrr/',
     #genotypes = "Analysis/hrr/goat_thin.ped",
     repo = "Documents/cremonesi/metabarcoding",
-    prjfolder = "Documents/cremonesi/nucleo_bro",
-    analysis_folder = "Analysis/micca",
+    prjfolder = "Documents/cremonesi/sonia_andres_cow_gut_microbiome",
+    analysis_folder = "Analysis",
     conf_file = "Config/mapping_file.csv",
-    suffix = "nucleo_bro",
-    nfactors = 2, ## n. of design variables (e.g. treatment and timpoint --> nfactors = 2)
+    suffix = "cow_feces",
+    nfactors = 1, ## n. of design variables (e.g. treatment and timpoint --> nfactors = 2)
     min_tot_n = 15,
     min_sample = 3,
     project = "", ##! use only for subsetting
     treatment_column = "treatment",
     sample_column = "sample",
-    grouping_variable2 = "timepoint",
+    grouping_variable2 = "",
     grouping_variable1 = "treatment",
     base_treatment = 1, ## reference level within timepoint (e.g. control)
     base_timepoint = 1, ## reference level within treatment (e.g. T0)
@@ -56,7 +56,8 @@ repo = file.path(HOME, config$repo)
 prjfolder = file.path(HOME, config$prjfolder)
 outdir = file.path(prjfolder,config$analysis_folder)
 
-fname = file.path(outdir, "alpha_comparison.config.RData")
+fname = paste("alpha_comparison.config_",config$suffix,".RData", sep="")
+fname = file.path(outdir, fname)
 save(config, file = fname)
 
 ## treatment levels as in the metadata file
@@ -76,7 +77,10 @@ if (config$project != "") metadata <- filter(metadata, project == !!config$proje
 print(head(metadata))
 
 metadata <- mutate(metadata, `sample-id` = as.character(`sample-id`))
-metadata = metadata |> select(c(`sample-id`, treatment, timepoint)) |> mutate(treatment = as.factor(treatment))
+metadata <- metadata |> filter(treatment != "", !is.na(treatment))
+if("timepoint" %in% names(metadata)) {
+  metadata = metadata |> select(c(`sample-id`, treatment, timepoint)) |> mutate(treatment = as.factor(treatment))
+} else metadata = metadata |> select(c(`sample-id`, treatment)) |> mutate(treatment = as.factor(treatment))
 
 ## read alpha div data
 alpha = fread(file.path(prjfolder, config$analysis_folder, "alpha.csv"))
@@ -89,7 +93,10 @@ alpha = alpha %>% inner_join(metadata, by = c("sample-id" = "sample-id"))
 alpha <- select(alpha, -c(se.chao1,se.ACE))
 
 ## reshaping data
-malpha = gather(alpha, key = "metric", value = "value", -c("sample-id",all_of(grouping_variable1),all_of(grouping_variable2)))
+if (grouping_variable2 != "") {
+  malpha = gather(alpha, key = "metric", value = "value", -c("sample-id",all_of(grouping_variable1),all_of(grouping_variable2)))
+} else malpha = gather(alpha, key = "metric", value = "value", -c("sample-id",all_of(grouping_variable1)))
+
 # malpha$treatment = factor(malpha$treatment, levels = c("PC","EU","non-EU+","non-EU-"))
 
 ## alpha div boxplots
@@ -97,24 +104,31 @@ p <- ggplot(malpha, aes(x = .data[[grouping_variable1]], y = value))
 p <- p + geom_boxplot(aes(color = .data[[grouping_variable1]]), alpha=0.7, width = 0.39, size = 1)
 p <- p + geom_jitter(aes(color = .data[[grouping_variable1]]), alpha = 0.8, width = 0.1)
 p <- p + scale_color_manual(values = c("#00AFBB", "#E7B800", "#FC4E07", "green","darkred"))
-p <- p + facet_grid(metric~.data[[grouping_variable2]], scales = "free")
+if (grouping_variable2 != "") {
+  p <- p + facet_grid(metric~.data[[grouping_variable2]], scales = "free")
+} else p <- p + facet_wrap(~metric, scales = "free")
 p <- p + theme(axis.text.x = element_text(angle = 0))
-# p
+p
 
 fname = paste("alpha_boxplot_", config$suffix, ".png", sep="")
 fname = file.path(outdir, "figures", fname)
 ggsave(filename = fname, plot = p, device = "png", width = 7.5, height = 8, dpi = 300)
 
 ## density plots
+if (grouping_variable2 != "") {
+  
 q <- ggplot(malpha, aes(y=.data[[grouping_variable2]], x=value, fill=.data[[grouping_variable1]])) +
   geom_density_ridges(scale=0.9, alpha = 0.5) +
   theme(legend.position="none") + facet_grid(.data[[grouping_variable2]]~metric, scales = "free")
 # q
+} else {
+  
+  q <- ggplot(malpha, aes(y=.data[[grouping_variable1]], x=value, fill=.data[[grouping_variable1]])) +
+    geom_density_ridges(scale=0.9, alpha = 0.5) +
+    theme(legend.position="none") + facet_wrap(~metric, scales = "free")
+} 
 
-q <- ggplot(malpha, aes(y=.data[[grouping_variable2]], x=value, fill=.data[[grouping_variable1]])) +
-  geom_density_ridges(scale=0.9, alpha = 0.5) +
-  theme(legend.position="none") + facet_wrap(~metric, scales = "free")
-# q
+print(q)
 
 fname = paste("alpha_plot_", config$suffix, ".png", sep="")
 fname = file.path(outdir, "figures", fname)
@@ -156,36 +170,46 @@ if (config$nfactors == 1) {
 filter(D, p.value < 0.05) |>
   print()
 
-alpha_stats <- malpha %>% 
-  group_by(metric, .data[[grouping_variable2]], .data[[grouping_variable1]]) %>%
-  summarise(avg = round(mean(value),3), std = round(sd(value),3))
+if (grouping_variable2 != "") {
+  
+  alpha_stats <- malpha %>% 
+    group_by(metric, .data[[grouping_variable2]], .data[[grouping_variable1]]) %>%
+    summarise(avg = round(mean(value),3), std = round(sd(value),3))
+} else {
+  
+  alpha_stats <- malpha %>% 
+    group_by(metric, .data[[grouping_variable1]]) %>%
+    summarise(avg = round(mean(value),3), std = round(sd(value),3))
+}
 
 alpha_stats[alpha_stats$avg > 10,"avg"] <- round(alpha_stats$avg[alpha_stats$avg > 10],1)
 alpha_stats[alpha_stats$std > 1,"std"] <- round(alpha_stats$std[alpha_stats$std > 1],1)
 
 dd <- alpha_stats |>
   unite("avg_", avg:std, sep = "+/-") |>
-  spread(key = treatment, value = avg_) |>
-  arrange(timepoint)
+  spread(key = treatment, value = avg_)
 
 fname = paste("alpha__treatment_stats", config$suffix, ".csv", sep="")
 dir.create(file.path(outdir, "tables"), showWarnings = FALSE)
 fname = file.path(outdir, "tables", fname)
 fwrite(dd, file = fname)
 
-avg_timepoint <- malpha |>
-  group_by(metric, timepoint) |>
-  summarise(avg = mean(value)) |>
-  rename(term = timepoint)
+if (grouping_variable2 != "") {
+  
+  avg_timepoint <- malpha |>
+    group_by(metric, timepoint) |>
+    summarise(avg = mean(value)) |>
+    rename(term = timepoint)
 
-temp <- avg_timepoint |>
-  rename(baseline = avg) |>
-  filter(term == base_timepoint) |>
-  select(-term)
+  temp <- avg_timepoint |>
+    rename(baseline = avg) |>
+    filter(term == base_timepoint) |>
+    select(-term)
 
-diff_timepoint <- avg_timepoint |>
-  left_join(temp, by = "metric") |>
-  mutate(diff = avg - baseline, pct_change = diff/baseline*100)
+  diff_timepoint <- avg_timepoint |>
+    left_join(temp, by = "metric") |>
+    mutate(diff = avg - baseline, pct_change = diff/baseline*100)
+}
 
 avg_treatment <- malpha |>
   group_by(metric, treatment) |>
@@ -201,10 +225,16 @@ diff_treatment <- avg_treatment |>
   left_join(temp, by = "metric") |>
   mutate(diff = avg - baseline, pct_change = diff/baseline*100)
 
-
-avg <- avg_timepoint |> bind_rows(avg_treatment)
-diff <- diff_timepoint |> bind_rows(diff_treatment) |>
-  select(-c(avg,baseline))
+if (grouping_variable2 != "") {
+  
+  avg <- avg_timepoint |> bind_rows(avg_treatment)
+  diff <- diff_timepoint |> bind_rows(diff_treatment) |>
+    select(-c(avg,baseline))
+} else {
+  
+  avg <- avg_treatment
+  diff <- diff_treatment
+}
 
 D$term  <- gsub("\\.data.*\\]","",D$term)
 
@@ -310,43 +340,73 @@ writeLines(" - making pairwise contrasts between treatments")
 ########################################
 ## MANUALLY SET THE X VARIABLE IN AOV()
 ########################################
-contrasts <- malpha |>
-  # nest(data = -c(timepoint,metric)) |>
-  nest(data = -c(metric, timepoint)) |>
-  mutate(
-    fit = map(data, ~ aov(value ~ treatment, data = .x)),
-    hsd = map(fit, TukeyHSD),
-    tidied = map(hsd, tidy)
-  ) |>
-  unnest(tidied) |>
-  select(-c(data,fit,hsd))
 
+if("timepoint" %in% names(malpha)) {
+
+  contrasts <- malpha |>
+  # nest(data = -c(timepoint,metric)) |>
+    nest(data = -c(metric, timepoint)) |>
+    mutate(
+      fit = map(data, ~ aov(value ~ treatment, data = .x)),
+      hsd = map(fit, TukeyHSD),
+      tidied = map(hsd, tidy)
+    ) |>
+    unnest(tidied) |>
+    select(-c(data,fit,hsd))
+} else {
+  
+  contrasts <- malpha |>
+    # nest(data = -c(timepoint,metric)) |>
+    nest(data = -c(metric)) |>
+    mutate(
+      fit = map(data, ~ aov(value ~ treatment, data = .x)),
+      hsd = map(fit, TukeyHSD),
+      tidied = map(hsd, tidy)
+    ) |>
+    unnest(tidied) |>
+    select(-c(data,fit,hsd))
+}
+  
 fname = paste("alpha_contrasts_", config$suffix, ".csv", sep="")
 fname = file.path(outdir, "tables", fname)
 fwrite(contrasts, file = fname)
 
-tmp <- contrasts |>
-  filter(!is.na(adj.p.value)) |>
-  select(metric, timepoint, contrast, adj.p.value) |>
-  mutate(pvalue = ifelse(adj.p.value <= 0.05, "<= 0.05", "> 0.05")) |>
-  select(-adj.p.value)
-
 colors = c("red","white")
 
-gg <- ggplot(tmp, aes(x = contrast, y = metric)) + geom_tile(aes(fill=pvalue), color = "white") + 
-  facet_wrap(~timepoint) + 
-  # scale_fill_distiller(palette = "YlGnBu") + 
-  scale_fill_manual(values=colors) + 
-  theme(axis.text.x = element_text(angle=90, size = 6),
-        # axis.text.y = element_text(size = 4),
-        # legend.key.size = unit(0.1, 'cm'),
-        # strip.text = element_text(size=7),
-        # legend.title = element_text(size=6),
-        # legend.text = element_text(size=6),
-        legend.key = element_rect(color="black"),
-        panel.background = element_rect(fill = 'white', color = 'white'),
-        panel.grid.major = element_line(color = 'white'),
-        panel.grid.minor = element_line(color = 'white'))
+if("timepoint" %in% names(malpha)) {
+  
+  tmp <- contrasts |>
+    filter(!is.na(adj.p.value)) |>
+    select(metric, timepoint, contrast, adj.p.value) |>
+    mutate(pvalue = ifelse(adj.p.value <= 0.05, "<= 0.05", "> 0.05")) |>
+    select(-adj.p.value)
+  
+  gg <- ggplot(tmp, aes(x = contrast, y = metric)) + geom_tile(aes(fill=pvalue), color = "white") + 
+    facet_wrap(~timepoint) + 
+    scale_fill_manual(values=colors) + 
+    theme(axis.text.x = element_text(angle=90, size = 6),
+          legend.key = element_rect(color="black"),
+          panel.background = element_rect(fill = 'white', color = 'white'),
+          panel.grid.major = element_line(color = 'white'),
+          panel.grid.minor = element_line(color = 'white'))
+} else {
+  
+  tmp <- contrasts |>
+    filter(!is.na(adj.p.value)) |>
+    select(metric, contrast, adj.p.value) |>
+    mutate(pvalue = ifelse(adj.p.value <= 0.05, "<= 0.05", "> 0.05")) |>
+    select(-adj.p.value)
+  
+  gg <- ggplot(tmp, aes(x = contrast, y = metric)) + geom_tile(aes(fill=pvalue), color = "white") + 
+    scale_fill_manual(values=colors) + 
+    theme(axis.text.x = element_text(angle=90, size = 6),
+          legend.key = element_rect(color="black"),
+          panel.background = element_rect(fill = 'white', color = 'white'),
+          panel.grid.major = element_line(color = 'white'),
+          panel.grid.minor = element_line(color = 'white'))
+}
+
+
 
 fname = paste("alpha_contrasts_significant_", config$suffix, ".png", sep="")
 fname = file.path(outdir, "figures", fname)
