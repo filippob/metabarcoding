2029214103632d59ab8937b976e2794609fec8a0
filippob <filippo.biscarini@gmail.com>
2025-07-28 14:42:15 +0200

    fixed typo with file names

--- micca/cca.R
+++ micca/cca.R
@@ -276,11 +276,11 @@ pCCA <- pCCA + theme(legend.position="none")
 # multiplot(pCCA,ph,cols=2)
 
 writeLines(" - saving results")
-fname = file.path(outdir, config$grouping_variable, "_cca_plot.png")
+fname = file.path(outdir, paste(config$grouping_variable, "cca_plot.png", sep="_"))
 g <- ggarrange(pCCA, ph, nrow = 1, common.legend = FALSE, labels = c("A", "B"), widths = c(0.45,0.55))
 ggsave(filename = fname, plot = g, device = "png", width = 9, height = 6, units = "in", dpi = 150)
 
-fname = file.path(outdir, config$grouping_variable, "_cca_results.txt")
+fname = file.path(outdir, paste(config$grouping_variable, "cca_results.txt"), sep="_")
 fwrite(x = V, file = fname)
 
 print("DONE!!")
